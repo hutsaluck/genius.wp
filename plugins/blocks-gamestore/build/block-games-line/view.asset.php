55820baa5289cd842584f64ae06c519a0d7c4fe3
Юра Гуцалюк <hutsaluck@gmail.com>
2025-06-12 09:47:11 +0300

    Add Woocommerce

---
+++ plugins/blocks-gamestore/build/block-games-line/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2e3eb9211fb25a10af62');
---
+++ plugins/blocks-gamestore/build/block-games-line/view.js
@@ -0,0 +1,20 @@
+/******/ (() => { // webpackBootstrap
+/*!**************************************!*\
+  !*** ./src/block-games-line/view.js ***!
+  \**************************************/
+document.addEventListener('DOMContentLoaded', () => {
+  const swiperGames = new Swiper('.games-line-container', {
+    loop: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    speed: 3500,
+    grabCursor: true,
+    mousewheelControl: true
+  });
+});
+/******/ })()
+;
+//# sourceMappingURL=view.js.map
